3b1edcb1291e6d9ef63ce0d4db4f13a364d83602
Misael M. Morales <misaelmiguelmorales@gmail.com>
2023-06-26 17:11:58 -0600

    Add files via upload

--- cushion/utils.py
+++ cushion/utils.py
@@ -10,12 +10,13 @@ from tqdm import tqdm
 
 from scipy.stats import zscore
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 import torch
 import torch.nn as nn
 from torch.nn import Linear, ReLU, LeakyReLU, Dropout1d, Dropout, BatchNorm1d
+from torch.nn.init import kaiming_uniform_, constant_
 import torch.optim as optim
 from torch.utils.data import TensorDataset, DataLoader
 from torch.utils.data.dataset import random_split
@@ -24,19 +25,16 @@ from torchviz import make_dot
 
 class h2_cushion_rom(nn.Module):
     def __init__(self, in_features, out_features, hidden_sizes=[100,60,10], 
-                 act=nn.LeakyReLU(0.33), drop=nn.Dropout(0.1), batchnorm_flag=True):
+                 act=nn.LeakyReLU(0.3), drop=nn.Dropout(0.1)):
         super(h2_cushion_rom, self).__init__()
         assert len(hidden_sizes) >= 1 , 'specify at least one hidden layer'
         layers = nn.ModuleList()
         layer_sizes = [in_features] + hidden_sizes
         for dim_in, dim_out in zip(layer_sizes[:-1], layer_sizes[1:]):
             layers.append(nn.Linear(dim_in, dim_out))
-            if act != None:
-                layers.append(act)
-            if drop != None:
-                layers.append(drop)
-            if batchnorm_flag:
-                layers.append(BatchNorm1d(dim_out))
+            layers.append(act)
+            layers.append(drop)
+            layers.append(BatchNorm1d(dim_out))
         self.layers = nn.Sequential(*layers)
         self.out_layer = Linear(hidden_sizes[-1], out_features)
     def forward(self, x):
@@ -58,8 +56,9 @@ class L1L2_Loss(nn.Module):
 class H2Toolkit:
     def __init__(self):
         self.return_data = False
+        self.return_plot = True
+        self.save_data   = True
         self.verbose     = True
-        self.save_result = True
         self.inp         = 12
         self.out         = 3
         self.xcols       = range(12)
@@ -68,10 +67,12 @@ class H2Toolkit:
         self.noise       = [0, 0.05]
         self.gwrt_cutoff = 1e5
         self.std_outlier = 3
-        self.valid_size  = 0.15
-        self.test_size   = 0.20
+        self.y_labels    = ['efft','ymft','gwrt']
+
+        self.test_size   = 0.25
         self.epochs      = 250
-        self.target_labels = ['efft','ymft','gwrt']
+        self.batch_size  = 100
+        self.monitor_epochs = 50
 
     def check_torch_gpu(self):
         version, cuda_avail = torch.__version__, torch.cuda.is_available()
@@ -109,126 +110,101 @@ class H2Toolkit:
         data_trunc = data_shuffle[data_shuffle['gwrt']<self.gwrt_cutoff]
         data_outl = data_trunc[(np.abs(zscore(data_trunc))<self.std_outlier)]
         if restype=='SA':
-            data_outl['Sw'] = 1
-        elif restype=='DGR':
-            data_outl['Sw'] = self.all_data['Sw']
-        data_clean = data_outl.dropna()  
-        X_data, y_data = data_clean.iloc[:, self.xcols], data_clean.iloc[:, self.ycols]
-        y_data_log = y_data.copy()
+            data_outl.loc[:,'Sw'] = 1
+        self.data_clean = data_outl.dropna()  
+        X_data, y_data = self.data_clean.iloc[:, self.xcols], self.data_clean.iloc[:, self.ycols]
+        y_data_log = pd.DataFrame(columns=y_data.columns)
+        y_data_log['efft'], y_data_log['ymft'] = y_data['efft'], y_data['ymft']
         y_data_log['gwrt'] = np.log10(y_data['gwrt'])
         self.x_scaler, self.y_scaler = MinMaxScaler(), MinMaxScaler()
         self.x_scaler.fit(X_data);   self.y_scaler.fit(y_data_log)
         self.X_dataset = self.x_scaler.transform(X_data)
         self.y_dataset = self.y_scaler.transform(y_data_log)
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X_dataset, self.y_dataset, test_size=self.test_size)
         if self.verbose:
             print('Full dataset shape:', self.all_data.shape)
             print('Truncated dataset shape:', data_trunc.shape)
-            print('Clean (no outliers) dataset shape:', data_clean.shape)
+            print('Clean (no outliers) dataset shape:', self.data_clean.shape)
             print('Dataset: X={} | y={}'.format(self.X_dataset.shape, self.y_dataset.shape))
+            print('X_train: {} | y_train: {}\nX_test:  {} | y_test:  {}'.format(self.X_train.shape, self.X_test.shape, self.y_train.shape, self.y_test.shape))
         if self.return_data:
-            return self.X_dataset, self.y_dataset, self.x_scaler, self.y_scaler
+            datasets = {'X_dataset':self.X_dataset, 'y_dataset':self.y_dataset}
+            train_test_data = {'X_train':self.X_train, 'X_test':self.X_test, 'y_train':self.y_train, 'y_test':self.y_test}
+            scalers = {'x_scaler':self.x_scaler, 'y_scaler':self.y_scaler}
+            return datasets, train_test_data, scalers 
         
-    def train_valid_test_split(self):
-        ntrain = int(np.floor(self.X_dataset.shape[0]*(1-self.test_size)))
-        nvalid = ntrain - int(np.floor(ntrain * self.valid_size))
-        self.X_train, self.y_train = self.X_dataset[:nvalid],       self.y_dataset[:nvalid]
-        self.X_valid, self.y_valid = self.X_dataset[nvalid:ntrain], self.y_dataset[nvalid:ntrain]
-        self.X_test,  self.y_test  = self.X_dataset[ntrain:],       self.y_dataset[ntrain:]
+    def load_data(self):
+        self.X_dataset = np.load('data/X_data.npy')
+        self.y_dataset = np.load('data/y_data.npy')
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X_dataset, self.y_dataset, test_size=self.test_size)
         if self.verbose:
-            print('X: train: {} | validation: {} | test: {}'.format(self.X_train.shape, self.X_valid.shape, self.X_test.shape))
-            print('y: train: {}  | validation: {}  | test: {}'.format(self.y_train.shape, self.y_valid.shape, self.y_test.shape))
+            print('Dataset: X={} | y={}'.format(self.X_dataset.shape, self.y_dataset.shape))
+            print('X_train: {} | y_train: {}\nX_test:  {} | y_test:  {}'.format(self.X_train.shape, self.X_test.shape, self.y_train.shape, self.y_test.shape))
         if self.return_data:
-            return self.X_train, self.X_valid, self.X_test, self.y_train, self.y_valid, self.y_test
-    
-    def arrays_to_tensors(self):
-        self.X_train_tensor = torch.tensor(self.X_train).float().to(self.device)
-        self.y_train_tensor = torch.tensor(self.y_train).float().to(self.device)
-        self.X_valid_tensor = torch.tensor(self.X_valid).float().to(self.device)
-        self.y_valid_tensor = torch.tensor(self.y_valid).float().to(self.device)
-        self.X_test_tensor = torch.tensor(self.X_test).float().to(self.device)
-        self.y_test_tensor = torch.tensor(self.y_test).float().to(self.device)
-        if self.return_data:
-            return self.X_train_tensor, self.X_valid_tensor, self.X_test_tensor, self.y_train_tensor, self.y_valid_tensor, self.y_test_tensor
-    
-    def make_dataloader(self, train_shuffle=True, train_batch=40, valid_shuffle=False, valid_batch=10):
-        self.train_dataset = TensorDataset(self.X_train_tensor, self.y_train_tensor)
-        self.valid_dataset = TensorDataset(self.X_valid_tensor, self.y_valid_tensor)
-        self.train_loader  = DataLoader(self.train_dataset, shuffle=train_shuffle, batch_size=train_batch)
-        self.valid_loader  = DataLoader(self.valid_dataset, shuffle=valid_shuffle, batch_size=valid_batch)
-        if self.return_data:
-            return self.train_dataset, self.train_loader, self.valid_dataset, self.valid_loader
-        
+            datasets = {'X_dataset':self.X_dataset, 'y_dataset':self.y_dataset}
+            train_test_data = {'X_train':self.X_train, 'X_test':self.X_test, 'y_train':self.y_train, 'y_test':self.y_test}
+            return datasets, train_test_data 
+            
     #################### ROM #################### 
-    def train_one_epoch(self, model, loss_func, optimizer):
-        model.train()
-        criterion = loss_func
-        loss_step = []
-        for (xbatch, ybatch) in self.train_loader:
-            yhat = model(xbatch)
-            loss = criterion(yhat, ybatch)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            with torch.no_grad():
-                loss_step.append(loss.item())
-            train_loss_epoch = torch.tensor(loss_step).mean().numpy()
-            return train_loss_epoch
-    
-    def validate(self, model, loss_func):
-        model.eval()
-        criterion = loss_func
-        v_loss_step = []
-        with torch.no_grad():
-            for xbatch, ybatch in self.valid_loader:
-                yhat = model(xbatch)
-                val_loss = criterion(yhat, ybatch)
-                v_loss_step.append(val_loss.item())
-            val_loss_epoch = torch.tensor(v_loss_step).mean().numpy()
-            return val_loss_epoch
-        
-    def train(self, model, loss_func, optimizer):
+    def train(self, model, loss_fn, optimizer, validation_split=0.2):
+        #tensorize
+        self.X_train_tensor = torch.Tensor(self.X_train).to(self.device)
+        self.X_test_tensor  = torch.Tensor(self.X_test).to(self.device)
+        self.y_train_tensor = torch.Tensor(self.y_train).to(self.device)
+        # Training loop
         model = model.to(self.device)
-        self.dict_log = {'loss':[], 'valid_loss':[]}
-        pbar = tqdm(range(self.epochs))
-        for epoch in pbar:
-            loss_epoch = self.train_one_epoch(model, loss_func, optimizer)
-            val_loss_epoch = self.validate(model, loss_func)
-            msg = 'Epoch: {}/{} -- Loss: {:.5f}, Validation Loss: {:.5f}'.format(epoch+1, self.epochs,
-                                                                              loss_epoch, 
-                                                                              val_loss_epoch)
-            pbar.set_description(msg)
-            self.dict_log['loss'].append(loss_epoch)
-            self.dict_log['valid_loss'].append(val_loss_epoch)
-        if self.return_data:
-            return self.dict_log
-
+        loss, validation_loss = [], []
+        self.fit = {'loss':[], 'validation_loss':[]}
+        start = time()
+        for epoch in range(self.epochs):
+            xtrain, xvalid, ytrain, yvalid = train_test_split(self.X_train_tensor, self.y_train_tensor, test_size=validation_split)
+            #training
+            model.train()
+            epoch_loss = 0.0
+            for i in range(0, len(xtrain), self.batch_size):
+                inp  = torch.Tensor(xtrain[i:i+self.batch_size]).to(self.device)
+                true = torch.Tensor(ytrain[i:i+self.batch_size]).to(self.device)
+                optimizer.zero_grad()
+                pred = model(inp)
+                loss = loss_fn(pred, true)
+                loss.backward()
+                optimizer.step()
+                epoch_loss += loss.item()*inp.size(0)
+            self.fit['loss'].append(epoch_loss/len(xtrain))
+            # validation
+            model.eval()
+            validation_loss = 0.0
+            with torch.no_grad():
+                for i in range(0, len(xvalid), self.batch_size):
+                    inp = torch.Tensor(xvalid[i:i+self.batch_size]).to(self.device)
+                    true = torch.Tensor(yvalid[i:i+self.batch_size]).to(self.device)
+                    pred = model(inp)
+                    validation_loss += loss_fn(pred, true).item()*inp.size(0)
+            self.fit['validation_loss'].append(validation_loss/len(xvalid))
+            if epoch%self.monitor_epochs==0:
+                print('Epoch [{}/{}], Train Loss: {:.4f}, Val Loss: {:.4f}'.format(epoch+1, self.epochs, epoch_loss/len(xtrain), validation_loss/len(xvalid)))
+        traintime = (time() - start)/60
+        n_params  = sum(p.numel() for p in model.parameters())
+        if self.verbose:
+            print('# Parameters: {} | Training time: {:.3f} minutes'.format(n_params, traintime))
+        if self.save_data:
+            torch.save(model.state_dict(), 'h2_cushion_rom.pt')            
+            
     def make_predictions(self, model):
+        model = model.to(self.device)
         self.y_train_pred = np.array(model(self.X_train_tensor).tolist())
         self.y_test_pred  = np.array(model(self.X_test_tensor).tolist())
-        print('Train Pred shape: {}'.format(self.y_train_pred.shape))
-        print('Test Pred shape:  {}'.format(self.y_test_pred.shape))
-        if self.return_data:
-            return self.y_train_pred, self.y_test_pred
+        return self.y_train_pred, self.y_test_pred
     
     #################### PLOTS & PRINTS ####################
-    def rescale_results(self):
-        def inv_scale(data):
-            return pd.DataFrame(self.y_scaler.inverse_transform(data), columns=data.columns)
-        self.y_train_r,      self.y_test_r      = inv_scale(self.y_train),      inv_scale(self.y_test)
-        self.y_train_pred_r, self.y_test_pred_r = inv_scale(self.y_train_pred), inv_scale(self.y_test_pred)
-        self.y_train_r['gwrt'], self.y_train_pred_r['gwrt'] = 10**self.y_train_r['gwrt'], 10**self.y_train_pred_r['gwrt']
-        self.y_test_r['gwrt'],  self.y_test_pred_r['gwrt']  = 10**self.y_test_r['gwrt'],  10**self.y_test_pred_r['gwrt']
-        if self.return_data:
-            return self.y_train_r, self.y_test_r, self.y_train_pred_r, self.y_test_pred_r
-    
     def print_metrics(self):
-        tot_train_r2,  tot_test_r2  = r2_score(self.y_train, self.y_train_pred),            r2_score(self.y_test, self.y_test_pred)
+        self.tot_train_r2,  self.tot_test_r2  = r2_score(self.y_train, self.y_train_pred),  r2_score(self.y_test, self.y_test_pred)
         tot_train_mse, tot_test_mse = mean_squared_error(self.y_train, self.y_train_pred),  mean_squared_error(self.y_test, self.y_test_pred)
         tot_train_mae, tot_test_mae = mean_absolute_error(self.y_train, self.y_train_pred), mean_absolute_error(self.y_test, self.y_test_pred)
-        print('TRAIN: R2={:.3f} | MSE={:.5f} | MAE={:.5f}'.format(tot_train_r2, tot_train_mse, tot_train_mae))
-        print('TEST:  R2={:.3f} | MSE={:.5f} | MAE={:.5f}'.format(tot_test_r2, tot_test_mse, tot_test_mae))     
+        print('TRAIN: R2={:.3f} | MSE={:.5f} | MAE={:.5f}'.format(self.tot_train_r2, tot_train_mse, tot_train_mae))
+        print('TEST:  R2={:.3f} | MSE={:.5f} | MAE={:.5f}'.format(self.tot_test_r2, tot_test_mse, tot_test_mae))     
         for i in range(3):
-            name = self.target_labels[i]
+            name = self.y_labels[i]
             trainr2,  testr2  = r2_score(self.y_train[:,i], self.y_train_pred[:,i]),            r2_score(self.y_test[:,i], self.y_test_pred[:,i])
             trainmse, testmse = mean_squared_error(self.y_train[:,i], self.y_train_pred[:,i]),  mean_squared_error(self.y_test[:,i], self.y_test_pred[:,i])
             trainmae, testmae = mean_absolute_error(self.y_train[:,i], self.y_train_pred[:,i]), mean_absolute_error(self.y_test[:,i], self.y_test_pred[:,i])
@@ -237,13 +213,13 @@ class H2Toolkit:
             print('{} TRAIN: MSE={:.5f} | TEST: MSE={:.5f}'.format(name, trainmse, testmse))   
             print('{} TRAIN: MAE={:.5f} | TEST: MAE={:.5f}'.format(name, trainmae, testmae))
         if self.save_data:
-            metrics = np.array([tot_train_r2, tot_test_r2, tot_train_mse, tot_test_mse, tot_train_mae, tot_test_mae])
+            metrics = np.array([self.tot_train_r2, self.tot_test_r2, tot_train_mse, tot_test_mse, tot_train_mae, tot_test_mae])
             np.save('data/metrics.npy', metrics)
             
     def plot_loss(self, title='', figsize=(4,3)):
         if figsize:
             plt.figure(figsize=figsize)
-        loss, val = self.dict_log['loss'], self.dict_log['valid_loss']
+        loss, val = self.fit['loss'], self.fit['validation_loss']
         epochs = len(loss)
         iterations = np.arange(epochs)
         plt.plot(iterations, loss, '-', label='loss')
@@ -251,36 +227,26 @@ class H2Toolkit:
         plt.title(title+' Training Loss vs epochs'); plt.legend()
         plt.xlabel('Epochs'); plt.ylabel('Loss'); plt.xticks(iterations[::epochs//10])
         plt.savefig('figures/training_performance.png')
-        plt.show()
+        if self.return_plot:
+            plt.show()
        
     def plot_results(self, figsize=(15,4), figname='Results'):
         plt.figure(figsize=figsize)
-        plt.suptitle(figname)
+        plt.suptitle(figname + ' -- Train $R^2$={:.2f} | Test $R^2$={:.2f}'.format(self.tot_train_r2, self.tot_test_r2))
         for i in range(3):
-            name = self.target_labels[i]
+            name = self.y_labels[i]
             r2train = r2_score(self.y_train[:,i], self.y_train_pred[:,i])
             r2test  = r2_score(self.y_test[:,i],  self.y_test_pred[:,i])
             plt.subplot(1,3,i+1)
-            plt.axline([0,0],[1,1], c='r', linewidth=3)
             plt.scatter(self.y_train[:,i], self.y_train_pred[:,i], alpha=0.5, label='train')
             plt.scatter(self.y_test[:,i],  self.y_test_pred[:,i],  alpha=0.5, label='test')
-            plt.xlabel('True'); plt.ylabel('Predicted'); plt.legend()
+            plt.axline([0,0],[1,1], c='r', linewidth=3); plt.legend()
+            plt.xlabel('True'); plt.ylabel('Predicted'); plt.xlim([-0.1,1.1]); plt.ylim([-0.1,1.1])
             plt.title('{} - $R^2_{}$={:.2f} ; $R^2_{}$={:.2f}'.format(name,'{train}',r2train,'{test}',r2test))
         plt.savefig('figures/' + figname + '.png')
-        plt.show()
-        
-    def save_data(self, model):
-        if self.save_result:
-            np.save('data/X_train.npy',self.X_train); np.save('data/y_train.npy',self.y_train)
-            np.save('data/X_valid.npy',self.X_valid); np.save('data/y_valid.npy',self.y_valid)
-            np.save('data/X_test.npy',self.X_test);   np.save('data/y_test.npy',self.y_test)
-            np.save('data/y_train_pred.npy', self.y_train_pred); 
-            np.save('data/y_test_pred.npy', self.y_test_pred)
-            torch.save(model.state_dict(), 'h2_cushion_rom.pt')
-            print('\nData is Saved! ..... DONE!')
-        else:
-            print('\n...DONE!')
-
+        if self.return_plot:
+            plt.show()
+            
 ########################################################################################################################
 ############################################################ END #######################################################
 ########################################################################################################################
