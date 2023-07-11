428a4de12942d3ed212472dc8e674308b6bd9316
Misael M. Morales <misaelmiguelmorales@gmail.com>
2023-07-11 14:29:26 -0600

    update

--- heterogeneity/main.py
+++ heterogeneity/main.py
@@ -9,5 +9,69 @@ from utils import *
 hete = Heterogeneity()
 hete.check_torch_gpu()
 
-facies = hete.load_facies()
-hete_fluv, hete_gaus = hete.load_perm_poro()
+#facies = hete.load_facies()
+#hete_fluv, hete_gaus = hete.load_perm_poro()
+
+################################################################
+
+n_realizations = 5
+n_timesteps    = 61
+dim            = 256
+
+pressure, sat_h2 = np.zeros((n_realizations,n_timesteps,dim,dim,1)), np.zeros((n_realizations,n_timesteps,dim,dim,1))
+poro,     perm   = np.zeros((n_realizations,dim,dim,1)),             np.zeros((n_realizations,dim,dim,1))
+
+for i in range(n_realizations):
+    data0 = loadmat('//dcstorage.lanl.gov/MFR2/misael/h2dataf/{}UHSS_0'.format(i+1))
+    poro[i], perm[i] = data0['PORO'], data0['PERMX']
+    for j in range(n_timesteps):
+        data = loadmat('//dcstorage.lanl.gov/MFR2/misael/h2dataf/{}UHSS_{}'.format(i+1,j))
+        pressure[i,j] = data['PRESSURE']
+        sat_h2[i,j]   = data['SGAS'] * data['YMF_3']
+        
+facies = np.expand_dims(hete.load_facies()[:n_realizations],-1)
+print(facies.shape)
+
+t_steps = np.arange(61)
+times = np.ones((n_realizations,n_timesteps,dim,dim,1))
+for i in range(n_timesteps):
+    times[:,i] = times[:,i]*t_steps[i]
+print(t_steps.shape, times.shape)
+
+X_train = np.concatenate([np.repeat(np.concatenate([poro,perm,facies], -1)[:,np.newaxis,...], n_timesteps, axis=1),times],-1).reshape(n_realizations*n_timesteps,dim,dim,4)
+y_train = np.concatenate([sat_h2, pressure],-1).reshape(n_realizations*n_timesteps,dim,dim,2)
+X_train = np.moveaxis(X_train, -1, 1)
+y_train = np.moveaxis(y_train, -1, 1)
+print('X: {} | y: {}'.format(X_train.shape, y_train.shape))
+
+device = 'cuda'
+
+rom = h2_hete_rom().to(device)
+optimizer = NAdam(rom.parameters(), lr=1e-3)
+loss_fn = nn.MSELoss()
+
+X_train = torch.Tensor(X_train).to(device)
+y_train = torch.Tensor(y_train).to(device)
+
+loss, val_loss = [], []
+metrics = {'loss':[], 'val_loss':[]}
+epochs = 10
+batch_size = 50
+
+for epoch in range(epochs):
+    rom.train()
+    epoch_loss = 0.0
+    for i in range(0, len(X_train), batch_size):
+        inp  = X_train[i:i+batch_size]
+        true = y_train[i:i+batch_size]
+        optimizer.zero_grad()
+        pred = rom(inp)
+        loss = loss_fn(pred,true)
+        loss.backward()
+        optimizer.step()
+        epoch_loss += loss.item()*inp.size(0)
+    metrics['loss'].append(epoch_loss/len(X_train))
+    
+np.save('metrics.npy', np.array(list(metrics.values())))
+dat = {'X':X_train, 'y':y_train, 'yhat':rom(X_train)}
+np.save('dat.npy', np.array(list(dat.values())))
--- heterogeneity/utils.py
+++ heterogeneity/utils.py
@@ -10,15 +10,20 @@ import pyvista as pv
 from time import time
 
 from scipy.stats import zscore
+from scipy.io import loadmat, savemat
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split, KFold
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 import torch
 import torch.nn as nn
+from torch.nn import Sequential
 from torch.nn import Linear, ReLU, LeakyReLU, Dropout, BatchNorm1d
+from torch.nn import Conv2d, ConvTranspose2d, MaxPool2d, BatchNorm2d
 from torch.optim import NAdam
+from torch.utils.data import DataLoader
 import torch.nn.functional as F
+from torchsummary import summary
 from torchviz import make_dot
 import torchio as tio
 
@@ -28,13 +33,13 @@ class Heterogeneity:
         self.ti_dir         = 'C:/Users/381792/Documents/MLTrainingImages/'
         self.n_realizations = 1000
         self.dim            = 256
-        self.standard_dims  = (1000,256,256)
+        self.standard_dims  = (1000, 256,256)
         self.flat_dims      = (1000, 256*256)
-        self.layers         = [48,64,80,96]
+        self.layers         = [48, 64, 80, 96]
         self.facies_range   = [0.75, 1.25]
-        self.fluv_range     = [0, 2.80]
-        self.gaus_range     = [0, 2.90]
-        self.lognormnoise   = [-5, 0.1]
+        self.fluv_range     = [0.00, 2.80]
+        self.gaus_range     = [0.00, 2.90]
+        self.lognormnoise   = [-5.0, 0.1]
         self.seed           = 424242
         self.save_data      = False
         self.verbose        = True
@@ -199,3 +204,55 @@ class Heterogeneity:
     #     p = np.expand_dims(poro_fluv.reshape(520,128,128,16), -1)
     #     hete_fluv = np.concatenate([k,p], -1)
     #     return None
+    
+def double_convolution(in_channels, out_channels):
+    conv_op = Sequential(
+        Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+        ReLU(inplace=True),
+        Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+        ReLU(inplace=True))
+    return conv_op
+
+class h2_hete_rom(nn.Module):
+    def __init__(self):
+        super(h2_hete_rom, self).__init__()
+        
+        self.max_pool2d = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.down_convolution_1 = double_convolution(4, 16)
+        self.down_convolution_2 = double_convolution(16, 32)
+        self.down_convolution_3 = double_convolution(32, 64)
+        self.down_convolution_4 = double_convolution(64, 128)
+        self.down_convolution_5 = double_convolution(128, 256)
+
+        self.up_transpose_1   = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.up_convolution_1 = double_convolution(256, 128)
+        self.up_transpose_2   = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.up_convolution_2 = double_convolution(128, 64)
+        self.up_transpose_3   = nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2)
+        self.up_convolution_3 = double_convolution(64, 32)
+        self.up_transpose_4   = nn.ConvTranspose2d(32, 16, kernel_size=2,  stride=2)
+        self.up_convolution_4 = double_convolution(32, 16)
+
+        self.out = nn.Conv2d(16, 2, kernel_size=1) 
+        
+    def forward(self, x):
+        down_1 = self.down_convolution_1(x)
+        down_2 = self.max_pool2d(down_1)
+        down_3 = self.down_convolution_2(down_2)
+        down_4 = self.max_pool2d(down_3)
+        down_5 = self.down_convolution_3(down_4)
+        down_6 = self.max_pool2d(down_5)
+        down_7 = self.down_convolution_4(down_6)
+        down_8 = self.max_pool2d(down_7)
+        down_9 = self.down_convolution_5(down_8)        
+        
+        up_1 = self.up_transpose_1(down_9)
+        x    = self.up_convolution_1(torch.cat([down_7, up_1], 1))
+        up_2 = self.up_transpose_2(x)
+        x    = self.up_convolution_2(torch.cat([down_5, up_2], 1))
+        up_3 = self.up_transpose_3(x)
+        x    = self.up_convolution_3(torch.cat([down_3, up_3], 1))
+        up_4 = self.up_transpose_4(x)
+        x    = self.up_convolution_4(torch.cat([down_1, up_4], 1))
+        out   = self.out(x)
+        return out
